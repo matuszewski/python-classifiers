d694347f79ca782608c29f8455d896515769c45c
Krzysztof Matuszewski <krzysiekmatuszewski@outlook.com>
2022-01-12 02:18:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -117,11 +117,11 @@ for i in range(100):
     kNB_acc += accuracy_score(kNB_prediction, test_labels)
     pass
 
-print(f"Drzewo decyzyjne {dtc_tree_acc}")
-print(f"Decyzyjny las losowy {rfc_acc}")
-print(f"Regresja logistyczna {l_acc}")
-print(f"SVC {s_acc}')
-print(f"Najbliżsi sąsiedzi {kNB_acc}")
+print("Drzewo decyzyjne " + dtc_tree_acc)
+print("Decyzyjny las losowy " + rfc_acc)
+print("Regresja logistyczna " + l_acc)
+print("SVC " + s_acc)
+print("Najbliżsi sąsiedzi " + kNB_acc)
 
 
 dokładnosc = np.array([dtc_tree_acc, rfc_acc, l_acc, s_acc, kNB_acc])
